f1100010937462dff92ffc564bfbfb1851a96b56
Cole <theodoco@oregonstate.edu>
2022-05-19 16:38:16 -0700

    added insert

--- cs340_bsg_for_sample_webapp.sql
+++ cs340_bsg_for_sample_webapp.sql
@@ -37,6 +37,11 @@ CREATE TABLE `customers` (
     `customer_address` varchar(255) NOT NULL,
     `customer_birthdate` date NOT NULL
 );
+INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, customer_email, customer_phone_number, customer_address, customer_birthdate) VALUES 
+    ('1','George','Immler','immler@massivehats.net','5039475829','ohio','1990/04/26'),
+    ('2','June','Elizabeth','elizabeth@greenthumb.net','5039412329','portland','1987/02/30'),
+    ('3','Harold','Shwab','gigachad@hidethepainharold.gov','5039400029','ohio','1990/04/26'),
+    ('4','Rachael','Breats','breats@iliketoshop.net','5039475425','new jersey','1984/01/19');
 
 
 --
