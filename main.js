4dedfdab3d092e44fa9b967989128bdcd41fc137
Cole <theodoco@oregonstate.edu>
2022-05-20 10:41:59 -0700

    changed the name of people to customers

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 app.set('mysql', mysql);
 app.use('/people_certs', require('./people_certs.js'));
-app.use('/people', require('./people.js'));
+app.use('/customers', require('./customers.js'));
 app.use('/planets', require('./planets.js'));
 app.use('/', express.static('public'));
 
