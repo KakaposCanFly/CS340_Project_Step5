16a459ea22b6f090593038dd0b6ace983f8f66f6
Cole <theodoco@oregonstate.edu>
2022-06-02 19:05:39 -0700

    added input guidance for dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_wolfordj',
-  password        : '7235',
-  database        : 'cs340_wolfordj'
+  user            : 'cs340_{your_onid}',
+  password        : '{your_MariaDB_password}',
+  database        : 'cs340_{your_onid}'
 });
 module.exports.pool = pool;
